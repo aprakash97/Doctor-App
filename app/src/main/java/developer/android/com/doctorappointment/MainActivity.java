de1c858c5375cd34258e650124f70eeb5cbe9c5a
IT17111652 <it17111652@my.sliit.lk>
2022-05-06 19:43:14 +0530

    PAtient Login & Registration Interfaces

---
+++ app/src/main/java/developer/android/com/doctorappointment/MainActivity.java
@@ -0,0 +1,23 @@
+package developer.android.com.doctorappointment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       
+    }
+
+
+}
---
+++ app/src/main/java/developer/android/com/doctorappointment/PatientRegisterActivity.java
@@ -0,0 +1,14 @@
+package developer.android.com.doctorappointment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PatientRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.patient_register);
+    }
+}
